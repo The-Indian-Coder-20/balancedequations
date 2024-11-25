3664d45e43c4d719535b87555bce49e09f0fe401
Shrish <untitledlofi.clean@gmail.com>
2024-11-25 21:27:03 +0900

    configuring input file, continue working on parsing parentheses

--- input.py
+++ input.py
@@ -3,8 +3,10 @@ import re
 reCount, prCount = 1, 1
 reactantsCount, productsCount = [], []
 tempWord = ""
-valid_equation = re.compile(r'^[A-Za-z0-9+ ()]+$')
+valid_equation = re.compile(r'^[A-Za-z0-9()]+$')
 mainKey = True
+tempCount = 0
+lastBracketIndex = 0
 
 while mainKey:
 
@@ -12,19 +14,43 @@ while mainKey:
 
     while key:
         reactant = input(f"What is reactant {reCount}? (...#): ")
+        i = 0
         if reactant != "":
             reCount += 1
-            for i in reactant:
-                if i.isalpha():
-                    tempWord += i
-            reactantsCount.append(tempWord)
-            tempWord = ""
-            if i in "123456789":
-                reactantsCount.append(reactant[reactant.index(i):])
+            while i <= len(reactant) - 1 :
+                if reactant[i].isalpha():
+                    print(tempWord)
+                    tempWord += reactant[i]
+                elif reactant[i] in "123456789":
+                    reactantsCount.append(tempWord)
+                    reactantsCount.append(reactant[i:])
+                    tempWord = ""
+                elif reactant[i] in "()":
+                    print(tempWord)
+                    reactantsCount.append(tempWord)
+                    tempWord = ""
+                print(reactantsCount)
+                if i <= len(reactant) - 1:
+                    if reactant[i] == "(":
+                        tempWord = ""
+                        tempReactant = reactant[i + 1:]
+                        for j in range(len(tempReactant)):
+                            if tempReactant[j].isalpha():
+                                tempWord += tempReactant[j]
+                            elif tempReactant[j] == ")":
+                                for x in range(len(tempWord)):
+                                    if tempWord[x].isupper():
+                                        tempCount = x
+                                        reactantsCount.append(tempWord[tempCount:x] + tempWord[x])
+                                        reactantsCount.append(tempReactant[j+1])
+                        tempCount = 0
+                        tempWord = ""
+                        i += (j + 1) 
+                i += 1
         else:
             key = False
             if len(reactantsCount) == 0:
-                print("No reactants entered. Please re-enter the chemical equation")
+                print("No reactants entered. Please enter a reactant.")
                 key = True
 
     key = True
@@ -43,9 +69,11 @@ while mainKey:
         else:
             key = False
             if len(productsCount) == 0:
-                print("No products entered. Please re-enter the chemical equation")
+                print("No products entered. Please enter a product.")
                 key = True
 
+
+    print(reactantsCount, productsCount)
     reactants, products = "", ""
 
     if all(valid_equation.fullmatch(item) for item in reactantsCount) and all(valid_equation.fullmatch(item) for item in productsCount):
@@ -86,7 +114,14 @@ while mainKey:
                         if i != len(productsCount) - 2:
                             products += " + "
         print(f"The entered equation is {reactants} => {products}")
-        mainKey = False
+        check = input("Is the entered equation correct? [y/n] ")
+        if check.lower() == "y":
+            print("Proceeding...")
+            mainKey = False
+        else:
+            print("Please re-enter the chemical equation")
+            reactantsCount, productsCount = [], []
+            reCount, prCount = 1, 1
     else:
         print("Invalid equation, make sure the elements are entered correctly.")
         reactantsCount, productsCount = [], []
