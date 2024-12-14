4ac8ede4a930c7bd7dbd6af070aad68aa8d2f3bb
Shrish <untitledlofi.clean@gmail.com>
2024-12-14 12:14:17 +0900

    Venv added@

--- matrixMaker.py
+++ matrixMaker.py
@@ -4,6 +4,8 @@ from input import reactantsList, productsList
 
 elementMatrix = []
 
+print(uniqueElementsCountList, uniqueElementsList, reactantsList, productsList)
+
 for i in range(len(uniqueElementsList)):
     elementMatrix.append([])
     for j in range(len(reactantsList)):
