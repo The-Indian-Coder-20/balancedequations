d5e60ba1a84e93604bcafdc0d2b9233543a39f37
The-Indian-Coder-20 <untitledlofi.clean@gmail.com>
2025-01-28 23:55:29 +0900

    Finished duplicate checker after running tests

--- duplicatechecker.py
+++ duplicatechecker.py
@@ -1,19 +1,16 @@
 from input import finalReactantsCount, finalProductsCount
-from counter import uniqueElementsList
 
 tempReactantList = []
-
-tempReactantsCount = finalReactantsCount
 
 #Duplicate checker only made to handle up to two duplicates in one compound e.g. C1H3C1O1O1H1
 
 for i in finalReactantsCount:
-    for j in range(len(i[j])):
+    for j in range(len(i)):
         if i.count(i[j]) <= 2 and i[j] not in tempReactantList:
             tempReactantList.append(i[j])
             tempReactantList.append(i[j + 1])
-        elif i[j] in tempReactantList:
+        elif i[j] in tempReactantList and type(i[j]) != int:
             tempReactantList[tempReactantList.index(i[j]) + 1] += i[j + 1]
-        else:
+        elif type(i[j]) == str:
             tempReactantList.append(i[j])
             tempReactantList.append(i[j + 1])
