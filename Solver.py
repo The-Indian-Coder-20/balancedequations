8993289f53a819ee7d6f5d9480fe8b76ccaf4f1b
The-Indian-Coder-20 <untitledlofi.clean@gmail.com>
2025-01-31 14:51:53 +0900

    Work

--- Solver.py
+++ Solver.py
@@ -1,5 +1,5 @@
-from counter import nonUsageMatrix
-from sympy import Matrix
+from sympy import Matrix, symbols
+from counter import nonUsageMatrix, uniqueElementsList
 
 matrix = Matrix(nonUsageMatrix)
 
@@ -8,5 +8,20 @@ rref_matrix = matrix.rref()
 finalMatrixCounts = []
 
 for j in range(len(rref_matrix[0])):
-    if rref_matrix[0][j] < 0:
+    print(rref_matrix[0][j])
+    if rref_matrix[0][j] != 1 and rref_matrix[0][j] != 0:
         finalMatrixCounts.append(rref_matrix[0][j])
+
+print(rref_matrix)
+print(finalMatrixCounts)
+
+t = symbols("t")
+
+for i in range(len(finalMatrixCounts)):
+    finalMatrixCounts[i] = finalMatrixCounts[i] * t
+
+if len(nonUsageMatrix) - 1 <= len(uniqueElementsList) - 1:
+    for i in range(((len(uniqueElementsList) - 1) - (len(nonUsageMatrix) - 1))):
+        finalMatrixCounts.append(t)
+
+print(finalMatrixCounts)
